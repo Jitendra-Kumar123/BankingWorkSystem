9a10409d57f7527c9ce731130a15ee943d68588e
Jitendra Kumar <jitendra0202006@gmail.com>
2026-06-20 16:18:41 +0530

    login controller for login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model.js");
 const jwt = require("jsonwebtoken");
+const emailService = require("../services/email.service.js");
 
 async function userRegisterController(req, res){
     const {email , userName, password } = req.body;
@@ -39,8 +40,53 @@ async function userRegisterController(req, res){
         }, 
         token
     })
+
+    await emailService.sendUserRegistrationEmail(user.email, user.name);
 }
+
+async function userLoginController(req, res){
+    const {email , password } = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(401).json({
+            message: "email or password is invalid"
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password)
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message: "email or password is invalid"
+        })
+    }
+
+    const token = jwt.sign({userId: user._id}, 
+        
+            process.env.JWT_SECRET
+        ,
+        {
+            expiresIn: '3d'
+        }
+    )
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        message: "user loggedin successfully",
+        user: {
+            userId: user._id,
+            email: user.email,
+            userName: user.userName
+        }, 
+        token
+    })
+}
+
 
 module.exports = {
     userRegisterController,
+    userLoginController
 }
